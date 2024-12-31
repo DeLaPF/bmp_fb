975d422d58a716c28468d441df2b5d237df01525
DeLaPF <de.alafia@gmail.com>
2024-12-30 19:37:06 -0500

    added a second window to confirm all is working as expected (current frame render does not rely on previous frame or pollute future frame)

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,18 @@
 void app(SDL_Window* window)
 {
     SDLEventHandler eH(window);
-    BitmapFramebuffer bfb(11, 11, 400, 400);
-    TextureWindow bfbDisp("Display", bfb.getTextureId());
+    BitmapFramebuffer bfb0(11, 11, 400, 400);
+    TextureWindow bfbDisp0("Display 0", bfb0.getTextureId());
+    BitmapFramebuffer bfb1(11, 11, 400, 400);
+    TextureWindow bfbDisp1("Display 1", bfb1.getTextureId());
 
-    // Draw pattern on bitmap
-    auto bitmap = bfb.getBitmap();
-    for (auto i = 0; i < bitmap->size(); i++) { bitmap->at(i) = i%2 == 0 ? 0x1F7AC4 : 0; }
-    bfb.updateBitmap();
+    // Draw pattern on bitmap(s)
+    auto bitmap0 = bfb0.getBitmap();
+    for (auto i = 0; i < bitmap0->size(); i++) { bitmap0->at(i) = i%2 == 0 ? 0x1F7AC4 : 0; }
+    bfb0.updateBitmap();
+    auto bitmap1 = bfb1.getBitmap();
+    for (auto i = 0; i < bitmap1->size(); i++) { bitmap1->at(i) = i%3 == 0 ? 0x1F7AC4 : 0; }
+    bfb1.updateBitmap();
 
     // Main loop
     while (!eH.windowShouldClose()) {
@@ -26,9 +31,12 @@ void app(SDL_Window* window)
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        bfbDisp.drawWindow();
-        if (bfbDisp.didResize()) { bfb.resizeRenderDim(bfbDisp.width(), bfbDisp.height()); }
-        bfb.render();
+        bfbDisp0.drawWindow();
+        if (bfbDisp0.didResize()) { bfb0.resizeRenderDim(bfbDisp0.width(), bfbDisp0.height()); }
+        bfb0.render();
+        bfbDisp1.drawWindow();
+        if (bfbDisp1.didResize()) { bfb1.resizeRenderDim(bfbDisp1.width(), bfbDisp1.height()); }
+        bfb1.render();
 
         renderImguiFrame();
         SDL_GL_SwapWindow(window);
